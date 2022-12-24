7fd487e8936f21620e62828dc19d365cde792096
Simons-Maddy <xiaochunfu@126.com>
2022-12-24 12:48:18 +0800

    build

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.prepare().then(() => {
     });
 
     server.listen(3000, () => {
-        console.log('server is running at http://localhost:3001')
+        console.log('server is running at http://localhost:3000')
     })
 })
 
